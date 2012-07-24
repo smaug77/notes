05db3d2233b2518802381516a8dc11ba4eca2ede
Smaug the Dragon <smaug@purple.home>
2012-07-24 10:00:25 -0400

    added static to urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -7,5 +7,4 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^core/', include('core.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r’^static/(?P.*)$’, ‘django.views.static.serve’, {‘document_root’: settings.STATIC_ROOT}),
 )
