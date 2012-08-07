47f36798a6fa91eea8402340ac74add3abddd3fd
Smaug the Dragon <smaug@purple.home>
2012-08-06 21:34:32 -0400

    stuff

--- local_to_hero.sh
+++ local_to_hero.sh
@@ -1,6 +1,6 @@
-python manage.py dumpdata core > temp.json
-git add temp.json
+python manage.py dumpdata core --format=yaml> temp.yaml
+git add temp.yaml
 git commit -m 'uploading to heroku'
 git push heroku
 heroku run python manage.py reset core
-heroku run python manage.py loaddata temp.json
+heroku run python manage.py loaddata temp.yaml
